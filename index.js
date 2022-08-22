26116937e5977177b5701ea1ef69830139afcdbf
Noahkimtai <noahkimtai24@gmail.com>
2022-08-22 18:14:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var customerName = 'bob';
 // Write your solution in this file!
+console.log(customerName)
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    window.bestCustomer ='not bob'
+
+}
+//console.log(bestCustomer)
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer ='boby' 
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'bobe'
+}
